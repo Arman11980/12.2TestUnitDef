46ccba48c0f58ae205727c112b202369a3377adb
Arman11980 <165942302+Arman11980@users.noreply.github.com>
2024-09-03 21:38:49 +0300

    Add files via upload

---
+++ TestUnit1.py
@@ -0,0 +1,59 @@
+import UnitDefTest
+import unittest
+
+class TournamentTest(unittest.TestCase):
+    is_frozen = True
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up for class"""
+        cls.all_results = {}
+
+    @classmethod
+    def tearDownClass(cls):
+        """Tear down for class"""
+
+        print("===================================")
+        out ={}
+        for key, value in cls.all_results.items():
+            for k, v in value.items():
+                out[k] = str(v)
+            print(out)
+
+    def setUp(self):
+        """Set up for test"""
+        self.usain = UnitDefTest.Runner("Усэйн", 10)
+        self.andrew = UnitDefTest.Runner("Андрей", 9)
+        self.nick = UnitDefTest.Runner("Ник", 3)
+        print("Set up for [" + self.shortDescription() + "]")
+
+    def test_start_tour_1(self):
+        """first run / первый забег"""
+        print("id: " + self.id())
+        tour = UnitDefTest.Tournament(90, self.usain, self.nick)
+        results = tour.start()
+        last_runner = list(results.values())
+        self.assertTrue(last_runner[-1] == "Ник")
+        self.all_results[self.shortDescription()] = results
+
+    def test_start_tour_2(self):
+        """second run / второй забег"""
+        print("id: " + self.id())
+        tour = UnitDefTest.Tournament(90, self.andrew, self.nick)
+        results = tour.start()
+        last_runner = list(results.values())
+        self.assertTrue(last_runner[-1] == "Ник")
+        self.all_results[self.shortDescription()] = results
+
+    def test_start_tour_3(self):
+        """third run / третий забег"""
+        print("id: " + self.id())
+        tour = UnitDefTest.Tournament(90, self.andrew, self.usain, self.nick)
+        results = tour.start()
+        last_runner = list(results.values())
+        self.assertTrue(last_runner[-1] == "Ник")
+        self.all_results[self.shortDescription()] = results
+
+
+if __name__ == "__main__":
+    unittest.main()
